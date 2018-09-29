eb5e2dceb2fb47e8d4a9f563da4c5f184dbc2e38
Viperidae <Viperidae>
2018-09-29 07:32:57 -0400

    Kernel is now mapped to 3GB

--- sys/i686/preinit.c
+++ sys/i686/preinit.c
@@ -5,32 +5,31 @@
 #include <sys/dev/textscreen.h>
 #include <sys/i686/multiboot.h>
 
+#define KERNEL_BASE 0xC0000000
+
 void *start_initramfs;
 
 void
 _preinit(multiboot_info_t *multiboot_hdr)
 {
-    extern uint32_t heap_start;
-
     extern void _init();
-
     extern struct device textscreen_device;
 
-    uint32_t initrd = *(uint32_t*)(multiboot_hdr->mods_addr);
-    uint32_t heap = *(uint32_t*)(multiboot_hdr->mods_addr + 4);
+    uint32_t initrd = KERNEL_BASE + *(uint32_t*)(KERNEL_BASE + multiboot_hdr->mods_addr);
+    uint32_t heap = KERNEL_BASE + *(uint32_t*)(KERNEL_BASE + multiboot_hdr->mods_addr + 4);
 
-    start_initramfs = (void*)initrd;
+    start_initramfs = (void*)(initrd);
 
-    brk((void*)heap);
-
+    brk((void*)(heap));
+    
     device_ioctl(&textscreen_device, TEXTSCREEN_SETFG, 0x0F);
 
     kset_output(&textscreen_device);
-
+    
     _init();
-    printf("initrd is at: %x\n", multiboot_hdr->mods_addr);
-    printf("kernel heap is at: %x\n", &heap_start);
-    printf("test: %s\n", (char*)initrd);
+
+    printf("initrd is at: %x\n", start_initramfs);
+    printf("kernel heap is at: %x\n", heap);
 
     if (kmain() != 0) {
         printf("Unable to boot kernel!\n");
--- sys/i686/startup.asm
+++ sys/i686/startup.asm
@@ -5,7 +5,18 @@ FLAGS       equ MBALIGN | MEMINFO
 MAGIC       equ 0x1BADB002
 CHECKSUM    equ -(MAGIC + FLAGS)
 
-section .multiboot
+KERNEL_VIRTUAL_BASE equ 0xC0000000
+KERNEL_PAGE_NUMBER equ (KERNEL_VIRTUAL_BASE >> 22)
+
+section .data
+align 0x1000
+boot_page_directory:
+    dd 0x00000083
+    times (KERNEL_PAGE_NUMBER - 1) dd 0
+    dd 0x00000083
+    times (1024 - KERNEL_PAGE_NUMBER - 1) dd 0
+
+section .text
 
 align 4
     dd MAGIC
@@ -21,25 +32,43 @@ align 4
     dd 0x00000000
     dd 0x00000000
 
-section .bootstrap_stack
-align 4
-
-stack_bottom:
-times 16386 db 0
-
-stack_top:
-
-section .text
-
 global _start
 
 _start:
+    mov ecx, (boot_page_directory - KERNEL_VIRTUAL_BASE)
+    mov cr3, ecx
+
+    mov ecx, cr4
+    or ecx, 0x00000010
+    mov cr4, ecx
+
+    mov ecx, cr0
+    or ecx, 0x80000000
+    mov cr0, ecx
+
+    lea ecx, [_start_virtual]
+    jmp ecx
+
+    ; move this
     mov esp, stack_top
     extern _preinit
     extern start_bss
     extern end_bss
     push ebx
 
+
+    call _preinit
+    cli
+    jmp $
+
+_start_virtual:
+    mov dword [boot_page_directory], 0
+    invlpg [0]
+    mov esp, stack_top
+    push eax
+    add ebx, KERNEL_VIRTUAL_BASE
+    push ebx
+    ; clear .bss before calling _preinit
     mov ebx, start_bss
 loop:
     mov [ebx], byte 0
@@ -49,4 +78,15 @@ loop:
 
     call _preinit
     cli
+    hlt
     jmp $
+
+section .bss
+align 4
+
+section .bootstrap_stack
+stack_bottom:
+times 16386 db 0
+
+stack_top:
+
