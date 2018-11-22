8ac3cfb98e4deb98dce778f31721c423953d1ec6
Viperidae <foobar@dev.null>
2018-11-22 18:40:12 -0500

    Re-implemented ls

--- bin/ls/main.c
+++ bin/ls/main.c
@@ -1,17 +1,22 @@
 #include <dirent.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-static void
-ls_pretty_print(DIR *dirp)
-{
-    struct dirent *dirent;
+struct ls_dirent {
+    struct dirent dirent;
+};
 
+static void
+ls_pretty_print(struct ls_dirent **contents, int nentries)
+{
     int entries = 0;
     int longest = 0;
 
-    while (dirent = readdir(dirp)) {
-        int len = strlen(dirent->d_name);
+    for (int i = 0; i < nentries; i++) {
+        struct ls_dirent *entry = contents[i];
+
+        int len = strlen(entry->dirent.d_name);
 
         if (len > longest) {
             longest = len;
@@ -20,35 +25,36 @@ ls_pretty_print(DIR *dirp)
         entries++;
     }
 
-    rewinddir(dirp);
-
     int cols = 80 / (longest + 2);
     int rows = entries / cols;
     int padding = longest + 2;
 
     int row = 0;
     int col = 0;
-    printf("We can have %d cols\n", cols);
-    while (dirent = readdir(dirp)) {
-        int len = strlen(dirent->d_name);
 
-        switch (dirent->d_type) {
+    for (int i = 0; i < nentries; i++) {
+        struct ls_dirent *entry = contents[i];
+
+        int len = strlen(entry->dirent.d_name);
+
+        switch (entry->dirent.d_type) {
             case DT_BLK:
-                printf("\033[0;36m");
+                printf("\033[0;33m");
                 break;
             case DT_CHR:
-                printf("\033[0;37m");
+                printf("\033[0;33m");
                 break;
             case DT_DIR:
-                printf("\033[0;32m");
+                printf("\033[1;34m");
+                break;
+            case DT_LNK:
+                printf("\033[0;36m");
                 break;
         }
 
-        printf("%s\033[0m", dirent->d_name);
+        printf("%s\033[0m", entry->dirent.d_name);
 
-        if (rows <= 1) {
-            printf(" ");
-        } else {
+        if (col != cols) {
             int spaces = padding - len;
 
             for (int i = 0; i < spaces; i++) {
@@ -70,22 +76,73 @@ ls_pretty_print(DIR *dirp)
     }
 }
 
+static void
+sort_dirents_alphabetical(struct ls_dirent **entries, int nentries)
+{
+    for (int i = 0; i < nentries; i++) {
+        for (int j = i + 1; j < nentries; j++) {
+            struct ls_dirent *entry1 = entries[i];
+            struct ls_dirent *entry2 = entries[j];
+
+            if (strcmp(entry1->dirent.d_name, entry2->dirent.d_name) > 0) {
+                entries[i] = entry2;
+                entries[j] = entry1;
+            }
+        }
+    }
+}
+
+static int
+read_dirents(DIR *dirp, struct ls_dirent **entries, int nentries)
+{
+    struct dirent *dirent;
+
+    int results = 0;
+
+    for (int n = 0; n < nentries && (dirent = readdir(dirp)); n++) {
+
+        if (dirent->d_name[0] != '.') {
+            struct ls_dirent *entry = (struct ls_dirent*)calloc(1, sizeof(struct ls_dirent));
+
+            memcpy(&entry->dirent, dirent, sizeof(struct dirent));
+
+            entries[results++] = entry;
+        }
+    }
+
+    return results;
+}
+
+static int
+list_dir(const char *path)
+{
+    DIR *dirp = opendir(path);
+
+    if (dirp) {
+        struct ls_dirent *entries[512];
+
+        int count = read_dirents(dirp, entries, 512);
+
+        sort_dirents_alphabetical(entries, count);
+
+        ls_pretty_print(entries, count);
+
+        closedir(dirp);
+
+        return 0;
+    }
+
+    return -1;
+}
+
 int
 main(int argc, const char *argv[])
 {
     DIR *dirp;
 
     if (argc > 1) {
-        dirp = opendir(argv[1]);
+        return list_dir(argv[1]);
     } else {
-        char cwd[512];
-        getcwd(cwd, 612);
-        dirp = opendir(cwd);
+        return list_dir(".");
     }
-
-    if (dirp) {
-        ls_pretty_print(dirp);
-        return 0;
-    }
-    return -1;
 }
