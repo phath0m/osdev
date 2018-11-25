315b8211e6730c242fbec2e2caa828881250b1f4
Viperidae <foobar@dev.null>
2018-11-25 09:04:07 -0500

    Added pipe() syscall

--- include/sys/kernel.h
+++ include/sys/kernel.h
@@ -1,6 +1,7 @@
 #ifndef SYS_KERNEL_H
 #define SYS_KERNEL_H
 
+#include <sys/vfs.h>
 #include <sys/device.h>
 
 int kmain();
@@ -8,5 +9,6 @@ void kputs(const char *str);
 void kset_output(struct device *dev);
 void panic(const char *fmt, ...);
 void shutdown();
+void create_pipe(struct file **files);
 
 #endif
