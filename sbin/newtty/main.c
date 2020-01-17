54e8f0954b0258b72bb8b3a4a9c8655b620ce2b3
phath0m <unphath0mabl3@gmail.com>
2020-01-17 13:35:46 -0800

    newtty: returns properly on error

--- sbin/newtty/main.c
+++ sbin/newtty/main.c
@@ -6,9 +6,18 @@
 int
 main(int argc, const char *argv[])
 {
+    if (argc != 2) {
+        fprintf(stderr, "usage: newtty <tty>\n");
+        return -1;
+    }
     const char *console = argv[1];
 
     int tty = open(console, O_RDWR);
+
+    if (!tty) {
+        perror("newtty");
+        return -1;
+    }
 
     for (int i = 0; i < 3; i++) {
         close(i);    
