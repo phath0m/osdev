3ae1bdaec488feaddc9ae088d5a494ef88b44883
phath0m <unphath0mabl3@gmail.com>
2020-01-17 16:04:42 -0800

    cat: will only print bytes read from input now

--- bin/cat/main.c
+++ bin/cat/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int
 main (int argc, char *argv[])
@@ -7,15 +8,17 @@ main (int argc, char *argv[])
         FILE *fp = fopen(argv[1], "r");
 
         if (fp) {
+            size_t nread;
             char buffer[1024];
-        
-            while (fread (buffer, 1, 1024, fp) > 0) {
-                printf ("%s", buffer);
+
+            while ((nread = fread(buffer, 1, 1024, fp)) > 0) {
+                fwrite(buffer, 1, nread, stdout);
             }
         
-            printf ("\n");
-            
+            puts(""); 
             return 0;
+        } else {
+            perror("cat");
         }
     }
     return -1;
