0a808d037af6fca682d2ab2486af44c4eb4e6518
phath0m <unphath0mabl3@gmail.com>
2020-01-25 12:52:16 -0800

    doit: remove list.c, dict.c, ini.c and switch to libcollections and libini

--- sbin/doit/main.c
+++ sbin/doit/main.c
@@ -1,12 +1,12 @@
 #include <dirent.h>
 #include <fcntl.h>
+#include <libini.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include "dict.h"
-#include "ini.h"
+#include <collections/dict.h>
 
 struct service {
     char *  name;
