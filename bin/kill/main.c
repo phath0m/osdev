ba288bbe1ac4c537f8580fe4fbe7d2ada18d2037
phath0m <unphath0mabl3@gmail.com>
2020-01-30 15:12:18 -0800

    implement kill command

---
+++ bin/kill/main.c
@@ -0,0 +1,44 @@
+#include <ctype.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdlib.h>
+
+
+static void
+usage()
+{
+    fprintf(stderr, "usage: kill [<sigpsec>] <pid>\n");
+}
+
+int
+main(int argc, const char *argv[])
+{
+    if (argc < 2) {
+        usage();
+        return -1;
+    }
+
+    int sig = SIGTERM;
+
+    if (!strncmp(argv[1], "-", 1)) {
+        
+        if (argc != 3) {
+            usage();
+            return -1;
+        }
+
+        const char *opt = &argv[1][1];
+
+        sig = atoi(&argv[1][1]);
+    }
+
+    int pid = atoi(argv[argc - 1]);
+
+    if (kill(pid, sig) != 0) {
+        perror("kill");
+        return -1;
+    }
+
+    return 0;
+}
