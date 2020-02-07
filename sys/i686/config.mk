a592adcb0c4f0dfd6d349275026babfa395a28f9
phath0m <unphath0mabl3@gmail.com>
2020-02-07 13:18:31 -0800

    attempt to make the build system a bit more sane by separating out the architecture dependent stuff

---
+++ sys/i686/config.mk
@@ -0,0 +1,8 @@
+AS=nasm
+CC=i686-elysium-gcc
+LD=i686-elysium-ld
+AR=i686-elysium-ar
+AFLAGS= -f elf32
+CFLAGS = -c -std=gnu99 --freestanding -g -Wall -Werror -nostartfiles -nodefaultlibs -DELYSIUM_TARGET="\"i686\""
+
+export AS CC LD AR AFLAGS CFLAGS
