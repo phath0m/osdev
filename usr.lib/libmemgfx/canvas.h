cfde7dcb73f7f222acea2378472707db5f0f7bec
Viperidae <root@devlan.dev>
2020-02-11 18:45:43 -0500

    libmemgfx: add missing function prototypes to header

--- usr.lib/libmemgfx/canvas.h
+++ usr.lib/libmemgfx/canvas.h
@@ -17,6 +17,8 @@ void canvas_destroy(canvas_t *canvas);
 void canvas_clear(canvas_t *canvas, color_t col);
 void canvas_fill(canvas_t *canvas, int x, int y, int width, int height, uint32_t col);
 void canvas_rect(canvas_t *canvas, int x, int y, int width, int height, uint32_t col);
+void canvas_putc(canvas_t *canvas, int x, int y, int ch, color_t col);
+void canvas_puts(canvas_t *canvas, int x, int y, const char *str, color_t col);
 void canvas_putpixels(canvas_t *canvas, int x, int y, int width, int height, color_t *colors);
 void canvas_putcanvas(canvas_t *canvas, int x, int y, canvas_t *other);
 
