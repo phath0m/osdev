1df51868f4a7877508e312bb78b3755f79f77138
Viperidae <root@devlan.dev>
2020-02-17 12:30:04 -0500

    libinput: changed how mouse input is decoded

--- usr.lib/libinput/mouse.c
+++ usr.lib/libinput/mouse.c
@@ -41,21 +41,36 @@ mouse_next_event(mouse_t *mouse, struct mouse_event *eventbuf)
 
     int buttons = mouse_state[0];
 
-    int x = mouse_state[1];
-    int y = mouse_state[2];
+    if ((buttons & 0xC0)) {
+        /* apparently this indicates an overflow with the X and Y values */
+        /* we will ignore the packet if this is set */
+        eventbuf->event = MOUSE_NOP;
+        return;
+    }
+
+    unsigned int x = mouse_state[1];
+    unsigned int y = mouse_state[2];
+
+    if ((buttons & 0x20)) {
+        y |= 0xFFFFFF00;
+    }
+
+    if ((buttons & 0x10)) {
+        x |= 0xFFFFFF00;
+    }
 
     mouse_event_t event = MOUSE_NOP;
 
     if (x != mouse->prev_x || y != mouse->prev_y) {
-        eventbuf->x = mouse->prev_x + x;
-        eventbuf->y = mouse->prev_y + y;
+        eventbuf->x = (int)x;
+        eventbuf->y = (int)y;//mouse->prev_y + y;
         event = MOUSE_MOVE;
 
         mouse->prev_x = x;
         mouse->prev_y = y;
     } else {
-        eventbuf->x = mouse->prev_x;
-        eventbuf->y = mouse->prev_y;
+        eventbuf->x = (int)x;//mouse->prev_x;
+        eventbuf->y = (int)y;//mouse->prev_y;
     }
 
     if ((buttons & 0x01)) {
