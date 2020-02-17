b6ba00b75981102615702943edc7a0dbb07bd2a4
Viperidae <root@devlan.dev>
2020-02-17 15:15:33 -0500

    newlib: default signals are now handled by kernel

--- usr.lib/newlib/libc/premain.c
+++ usr.lib/newlib/libc/premain.c
@@ -7,21 +7,6 @@
 extern void exit(int code);
 extern int main(int argc, const char *argv[], int envc, const char *envp[]);
 
-static void
-default_exit_handler(int sig)
-{
-    switch(sig) {
-        case SIGSEGV:
-            fprintf(stderr, "Segmentation Fault\n");
-            break;
-        case SIGILL:
-            fprintf(stderr, "Illegal Instruction\n");
-            break;
-    }
-    exit(-1);
-}
-
-
 void
 _premain(int argc, const char **argv, int envc, const char **envp)
 {
@@ -36,11 +21,6 @@ _premain(int argc, const char **argv, int envc, const char **envp)
     for (i = 0; i < envc; i++) {
         environ[i] = envp[i];
     }
-
-    signal(SIGILL, default_exit_handler);
-    signal(SIGINT, default_exit_handler);
-    signal(SIGSEGV, default_exit_handler);
-    signal(SIGTERM, default_exit_handler);
 
     _init();
 
