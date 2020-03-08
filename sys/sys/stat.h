87c692b011126d41594a5609f67a8097650e9467
phath0m <unphath0mabl3@gmail.com>
2020-03-08 14:27:41 -0500

    kernel: stat.h, stat.h matches the struct defined by newlib's headers

--- sys/sys/stat.h
+++ sys/sys/stat.h
@@ -32,19 +32,23 @@
 #define S_ISREG(m)  (((m)&S_IFMT) == S_IFREG)
 
 struct stat {
-    dev_t           st_dev;
-    ino_t           st_ino;
-    mode_t          st_mode;
-    nlink_t         st_nlink;
-    uid_t           st_uid;
-    gid_t           st_gid;
-    dev_t           st_rdev;
-    uint32_t        st_size;
-    time_t          st_atime;
-    uint64_t        st_spare1;
-    time_t          st_mtime;
-    uint64_t        st_spare2;
-    time_t          st_ctime;
+    dev_t       st_dev;
+    ino_t       st_ino;
+    mode_t      st_mode;
+    nlink_t     st_nlink;
+    uid_t       st_uid;
+    gid_t       st_gid;
+    dev_t       st_rdev;
+    off_t       st_size;
+    time_t      st_atime;
+    long        st_spare1;
+    time_t      st_mtime;
+    long        st_spare2;
+    time_t      st_ctime;
+    long        st_spare3;
+    int         st_blksize;
+    int         st_blocks;
+    long        st_spare4[2];
 };
 
 #endif
