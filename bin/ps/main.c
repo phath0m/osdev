c23de445a48b5d8ef1e6f1b7e22896e54638ce0d
phath0m <unphath0mabl3@gmail.com>
2020-03-09 20:46:54 -0500

    ps: now uses sysctl() to get pslist

--- bin/ps/main.c
+++ bin/ps/main.c
@@ -5,29 +5,13 @@
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
-#include <collections/list.h>
-#include <sys/kernlink.h>
-#include <sys/socket.h>
+#include <sys/sysctl.h>
 
-struct ps_entry {
-    int     pid;
-    int     ppid;
-    int     gid;
-    int     uid;
-    int     sid;
-    int     pgid;
-    char    cmd[256];
-    char    tty[32];
-    time_t  stime;
-    char *  argv;
-    char *  envp;
-};
-
-struct ps_options {
-    bool    display_all;
-    bool    display_extended;
-    bool    display_all_terminals;
-};
+#define PS_DISPLAY_ALL      0x01
+#define PS_DISPLAY_ALL_TTYS 0x02
+#define PS_FORMAT_EXTENDED  0x04
+#define PS_FORMAT_JOB_CTL   0x08
+#define PS_FORMAT(options) ((options) & 0xC)
 
 static void
 get_time_string(char *buf, size_t buf_size, time_t stime)
@@ -44,15 +28,13 @@ get_time_string(char *buf, size_t buf_size, time_t stime)
 }
 
 static void
-ps_print_basic(struct ps_entry *entry)
+ps_print_basic(struct kinfo_proc *entry)
 {
-    printf("%5d ", entry->pid);
-    printf("%-10s ", entry->tty);
-    printf("%s\n", entry->cmd);
+    printf("%5d %-10s %s\n", entry->pid, entry->tty, entry->cmd);
 }
 
 static void
-ps_print_extended(struct ps_entry *entry)
+ps_print_extended(struct kinfo_proc *entry)
 {
     char time_buf[10];
 
@@ -73,159 +55,86 @@ ps_print_extended(struct ps_entry *entry)
 }
 
 static void
-ps_print_procs(struct ps_options *options, struct list *listp)
+ps_print_procs(int options, struct kinfo_proc *procs, int nprocs)
 {
-    if (options->display_extended) {
-        printf("UID         PID  PPID STIME TTY        CMD\n");
-    } else {
-        printf("  PID TTY        CMD\n");
+    switch (PS_FORMAT(options)) {
+        case PS_FORMAT_EXTENDED:
+            puts("UID         PID  PPID STIME TTY        CMD");
+            break;
+        default:
+            puts("  PID TTY        CMD");
+            break;
     }
 
     pid_t sid = getsid(0);
+    bool show_all = (options & PS_DISPLAY_ALL) != 0;
 
-    list_iter_t iter;
+    for (int i = 0; i < nprocs; i++) {
+        struct kinfo_proc *entry = &procs[i];
 
-    list_get_iter(listp, &iter);
-
-    struct ps_entry *entry;
-
-    while (iter_move_next(&iter, (void**)&entry)) {
-        if (!options->display_all && entry->sid != sid) {
+        if (!show_all && entry->sid != sid) {
             continue;
         }
 
-        if (options->display_extended) {
-            ps_print_extended(entry);
-        } else {
-            ps_print_basic(entry);
+        switch (PS_FORMAT(options)) {
+            case PS_FORMAT_EXTENDED:
+                ps_print_extended(entry);
+                break;
+            default:
+                ps_print_basic(entry);
+                break;
         }
     }
 }
 
-static int
-parse_arguments(struct ps_options *options, int argc, char *argv[])
+int
+main(int argc, char *argv[])
 {
+    int options = 0;
     int c;
-
     while (optind < argc) {
         if ((c = getopt(argc, argv, "aefA")) != -1) {
             switch (c) {
                 case 'a':
-                    options->display_all_terminals = true;
+                    options |= PS_DISPLAY_ALL_TTYS;
                     break;
                 case 'e':
                 case 'A':
-                    options->display_all = true;
+                    options |= PS_DISPLAY_ALL;
                     break;
                 case 'f':
-                    options->display_extended = true;
+                    options |= PS_FORMAT_EXTENDED;
                     break;
                 case '?':
                     return -1;
             }
         }
     }
-    
-    return 0;
-}
 
-static int
-klink_query(int sfd, int what, int item, struct klink_dgram *resp, size_t len)
-{
-    struct klink_dgram req;
+    int oid[3];
+    oid[0] = CTL_KERN;
+    oid[1] = KERN_PROC;
+    oid[2] = KERN_PROC_ALL;
 
-    req.opcode = KLINK_QUERY;
-    req.what = what;
-    req.item = item;
-    req.size = 0;
+    size_t bufsize = 0;
 
-    if (write(sfd, &req, sizeof(struct klink_dgram)) < 0) {
+    if (sysctl(oid, 3, NULL, &bufsize, NULL, 0)) {
+        perror("sysctl");
         return -1;
     }
 
-    int ret = read(sfd, resp, len);
+    struct kinfo_proc *procs = calloc(1, bufsize);
 
-    if (ret < sizeof(struct klink_dgram)) {
+    if (sysctl(oid, 3, procs, &bufsize, NULL, 0)) {
+        perror("sysctl");
         return -1;
     }
 
-    if (resp->what == KWHAT_ERROR) {
-        return -1;
-    }
+    int nprocs = bufsize / sizeof(struct kinfo_proc);
 
-    return 0;
-}
+    ps_print_procs(options, procs, nprocs);
 
-static int
-read_procs(struct list *listp)
-{
-    int sfd = socket(AF_KLINK, 0, SOCK_SEQPACKET);
-
-    if (sfd < 0) {
-        return -1;
-    }
-
-    if (connect(sfd, 0, 0)) {
-        return -1;
-    }
-
-    struct klink_dgram *proclist = (struct klink_dgram*)malloc(65535);
-
-    klink_query(sfd, KWHAT_PROCLIST, 0, proclist, 65535);
-
-    int nprocs = proclist->size / sizeof(struct klink_proc_info);
-
-    struct klink_proc_info *procs = (struct klink_proc_info*)(proclist + 1);
-
-    for (int i = 0; i < nprocs; i++) {
-        struct klink_proc_info *proc = &procs[i];
-
-        struct ps_entry *entry = (struct ps_entry*)malloc(sizeof(struct ps_entry));
-        entry->pid = proc->pid;
-        entry->ppid = proc->ppid;
-        entry->gid = proc->gid;
-        entry->uid = proc->uid;
-        entry->sid = proc->sid;
-        entry->pgid = proc->pgid;
-
-        struct klink_dgram pstat[sizeof(struct klink_dgram)+sizeof(struct klink_proc_stat)];
-        klink_query(sfd, KWHAT_PROCSTAT, entry->pid, pstat, sizeof(pstat));
-
-        struct klink_proc_stat *stat = (struct klink_proc_stat*)(pstat + 1);
-        entry->stime = stat->stime;
-        strncpy(entry->cmd, stat->cmd, 255);
-        strncpy(entry->tty, stat->tty, 32);
-
-        list_append(listp, entry);
-    }
-
-    free(proclist);
-
-    close(sfd);
-
-    return nprocs;
-}
-
-int
-main(int argc, char *argv[])
-{
-    struct ps_options options;
-
-    memset(&options, 0, sizeof(struct ps_options));
-
-    if (parse_arguments(&options, argc, argv)) {
-        return -1;
-    }
-
-    struct list proclist;
-
-    memset(&proclist, 0, sizeof(proclist));
-
-    read_procs(&proclist);
-
-    ps_print_procs(&options, &proclist);
-
-    list_destroy(&proclist, true);
+    free(procs);
 
     return 0;
 }
