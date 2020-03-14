2a86124b74406f62dbf946c223c05c3f7cc1c76f
phath0m <unphath0mabl3@gmail.com>
2020-03-14 17:06:23 -0500

    add date command to retrieve/modify system time

---
+++ bin/date/date.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <sys/time.h>
+
+static int
+settime(const char *timestr)
+{
+    time_t now = time(NULL);
+    struct tm tm;
+    localtime_r(&now, &tm);
+
+    size_t len = strlen(timestr);
+
+    int res = -1;
+
+    switch (len) {
+        case 4: /* HHmm */
+            res = sscanf(timestr, "%02d%02d", &tm.tm_hour, &tm.tm_min);
+            break;
+        case 8: /* YYYYMMDD */
+            res = sscanf(timestr, "%04d%02d%02d",
+                    &tm.tm_year, &tm.tm_mon, &tm.tm_mday);
+            tm.tm_year -= 1900; /* struct tm is stupid and stores the year relative to 1900 */
+            tm.tm_mon -= 1; /* month is zero based */
+            break;
+        case 12: /* YYYYMMDDHHmm*/       
+            res = sscanf(timestr, "%04d%02d%02d%02d%02d",
+                    &tm.tm_year, &tm.tm_mon, &tm.tm_mday, &tm.tm_hour,
+                    &tm.tm_min);
+            tm.tm_year -= 1900; /* struct tm is stupid and stores the year relative to 1900 */
+            tm.tm_mon -= 1; /* month is zero based */
+            break;
+    }
+
+    if (res <= 0) {
+        fprintf(stderr, "date: invalid date %s\n", timestr); 
+        return -1;
+    }
+
+    struct timeval tv;
+    tv.tv_sec = mktime(&tm);
+    tv.tv_usec = 0;
+
+    if (settimeofday(&tv, NULL) != 0) {
+        perror("date");
+        return -1;
+    }
+
+    return 0;
+}
+
+int
+main(int argc, char *argv[])
+{
+    time_t now = time(NULL);
+
+    if (argc == 1) {
+        struct tm *nowp = gmtime(&now);
+        fputs(asctime(nowp), stdout);
+        return 0;
+    }
+   
+    settime(argv[1]);
+
+    return 0;
+}
+
